7543c4ec444e468726755029ba06f9306a58488f
nikita <nikita1999@gmail.com>
2026-03-09 00:21:10 +0530

    using bcrypt for auth and hash the password

--- src/models/User.js
+++ src/models/User.js
@@ -10,11 +10,19 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, 'email is required'],
-    
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please provide a valid email',
+    ],
   },
   password: {
     type: String,
     required: [true, 'password is required'],
+    minlength: 6,
+    select: false,
   },
   createdAt: {
     type: Date,
@@ -22,6 +30,15 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model("User", userSchema);
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
-module.exports = User;
+
+
+module.exports = mongoose.model('User', userSchema);
