0fb6e2a23898910fd0f3d96baea3fef0ce6296cd
nikita <nikita1999@gmail.com>
2026-03-09 19:41:21 +0530

    Created Login logic and with route

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -19,5 +19,18 @@ exports.register = async (req, res) => {
 exports.login = async (req, res) => {
   const { email, password } = req.body;
 
-  
+  const user = await User.findOne({ email });
+
+  if (!user) return res.status(400).json({ message: "User not found" });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) return res.status(400).json({ message: "Invalid password" });
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+  res.json({
+    message: "User logged in successfully",
+    token: token
+  });
 };
