a8d085b7d61b7de047e125c16b523594a3722327
nikita <nikita1999@gmail.com>
2026-03-09 19:37:27 +0530

    registration logic created with hash password

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,6 +5,15 @@ const bcrypt = require("bcryptjs");
 exports.register = async (req, res) => {
   const { name, email, password } = req.body;
 
+  const hash = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hash
+  });
+
+  res.json(user);
 };
 
 exports.login = async (req, res) => {
