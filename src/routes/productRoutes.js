8f90d496e34200ac1b8edacc4c1573bbf0da479c
nikita <nikita1999@gmail.com>
2026-03-09 20:21:18 +0530

    added the function in product routes

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -2,14 +2,16 @@ const router = require("express").Router();
 
 const {
   addProduct,
-  } = require("../controllers/productController");
-
+  getProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct
+} = require("../controllers/productController");
 
 router.get("/products", getProducts);
 router.post("/product", addProduct);
 router.get("/products/:id", getProductById);
 router.put("/product/:id", updateProduct);
 router.delete("/product/:id", deleteProduct);
-
 
 module.exports = router;
